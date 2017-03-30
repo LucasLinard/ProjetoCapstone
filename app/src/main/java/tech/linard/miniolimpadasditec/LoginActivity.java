0f3bffb5c7e4a0f733ccbdef5b4ba0dbc6a388eb
Lucas Linard <contato@linard.tech>
2017-03-29 22:34:05 -0300

    Firebase dependencies

--- app/src/main/java/tech/linard/miniolimpadasditec/LoginActivity.java
+++ app/src/main/java/tech/linard/miniolimpadasditec/LoginActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // Firebase Authentication
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
